dd7f469b01fb3ed0eadffba034ee8d24ae2bdcd6
AJ <>
2026-01-25 14:10:09 +1100

    fix typo

--- app/src/main/java/com/aamsco/awardswithfriends/ui/competition/CompetitionDetailScreen.kt
+++ app/src/main/java/com/aamsco/awardswithfriends/ui/competition/CompetitionDetailScreen.kt
@@ -395,7 +395,7 @@ fun CompetitionDetailScreen(
             onDismissRequest = { showLeaveDialog = false },
             title = { Text("Leave Competition") },
             text = {
-                Text("Are you sure you want to leave \"${competition?.name}\"? Your votes will be deleted.")ye
+                Text("Are you sure you want to leave \"${competition?.name}\"? Your votes will be deleted.")
             },
             confirmButton = {
                 TextButton(
